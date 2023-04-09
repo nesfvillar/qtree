71e543f0e7a5a09aef708e7a212dca9dcbe42db3
Nestor Fabian Villar <nesfvillar@gmail.com>
2023-04-09 20:31:29 -0300

    CMakeLists creation and testing qtree

---
+++ test/qtree_test.cpp
@@ -0,0 +1,35 @@
+#include "qtree.hpp"
+
+#include <iostream>
+
+using namespace qtree;
+
+int main()
+{
+    QTree::Square boundary(Point<2>({ 0, 0 }), 1.0);
+    QTree qt(1, boundary);
+
+    std::initializer_list<Point<2>> list =
+    {
+        Point<2>({0.5, 0.5}),
+        Point<2>({-0.5, 0.25}),
+        Point<2>({0.7, -0.1}),
+        Point<2>({0.9, 0.7}),
+        Point<2>({-0.7, -0.9}),
+        Point<2>({0.5, -0.2})
+    };
+
+    for (const auto& p : list)
+    {
+        qt.insert(p);
+    }
+
+    auto result = qt.query_range(boundary);
+    for (const auto& r : result)
+    {
+        std::cout << r << '\n';
+    }
+
+    return 1;
+
+}
